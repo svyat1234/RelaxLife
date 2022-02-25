246548b7eac27269e1ffa5ffecc3589fae6e3b2f
svyat1234 <75384046+svyat1234@users.noreply.github.com>
2022-02-25 13:54:34 +0300

    Add files via upload

--- src/modules/sliderTabsAdapt.js
+++ src/modules/sliderTabsAdapt.js
@@ -1,37 +1,37 @@
 const sliderTabsAdapt = () => {
-    // const slider = document.querySelector('.nav-wrap-repair')
-    // const slides = slider.querySelectorAll('.button_o')
-    // console.log(slider);
-    // let correntSlide = 0
+    const slider = document.querySelector('.nav-wrap-repair')
+    const sliderLine = slider.querySelector('.nav-list-repair')
+    const slides = slider.querySelectorAll('.button_o')
+    const next = slider.querySelector('#nav-arrow-repair-right_base')
+    const prev = slider.querySelector('#nav-arrow-repair-left_base')
+    let correntSlide = 0
 
-    //  const prevSlide = (elems, index) => {
-    //     elems[index].style.display = "none"
-    // }
-
-    // const nextSlide = (elems, index) => {
-    //     elems[index].style.display = "flex"
-    // }
-
-    // slider.addEventListener('click', (e) => {
-    //     prevSlide(slides, correntSlide)
-    //     if (e.target.closest('#nav-arrow-repair-right_base')) {
-    //         correntSlide++
-            
-    //     } 
-    //     if (e.target.closest('#nav-arrow-repair-left_base')) {
-    //         correntSlide--
-    //     }
-    //     if (correntSlide >= slides.length) {
-    //         correntSlide = 0
-    //     } 
-    //     if (correntSlide < 0) {
-    //         correntSlide = slides.length - 1
-    //         slides.forEach(slide => {
-    //             slide.style.display = "none"
-    //         })
-    //     } 
-    //     nextSlide(slides, correntSlide)
-    // })
+    let offset = 0
+    if (offset <= 0) {
+        prev.style.display = "none"
+    }
+    slider.addEventListener('click', (e) => {
+        if (e.target.closest('#nav-arrow-repair-right_base')) {
+            offset = offset + slides[correntSlide].clientWidth + 13
+            correntSlide++
+            if (offset >= 704) {
+                next.style.display = "none"
+            } else {
+                prev.style.display = "block"
+            }
+            sliderLine.style.marginLeft = -offset + 'px'
+        } 
+        if (e.target.closest('#nav-arrow-repair-left_base')) {
+            correntSlide--
+            offset = offset - slides[correntSlide].clientWidth - 13
+            if (offset <= 0) {
+                prev.style.display = "none"
+                correntSlide = 0
+            } else {
+                next.style.display = "block"
+            }
+            sliderLine.style.marginLeft = -offset + 'px'
+        }
+    })
 }
-
 export default sliderTabsAdapt
